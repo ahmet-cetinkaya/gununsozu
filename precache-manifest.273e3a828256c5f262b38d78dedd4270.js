20b52c0247c9d614919716b6634ab0e129395b2f
Ahmet Çetinkaya <53148314+ahmet-cetinkaya@users.noreply.github.com>
2020-08-24 20:12:27 +0300

    Build

---
+++ precache-manifest.273e3a828256c5f262b38d78dedd4270.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "274adb0add49cfa1cd468d9543824e84",
+    "url": "./index.html"
+  },
+  {
+    "revision": "972aa5111dd20aeb56e5",
+    "url": "./static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "576ba5972d941b252588",
+    "url": "./static/js/2.dfc1031c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.dfc1031c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972aa5111dd20aeb56e5",
+    "url": "./static/js/main.d6b41c91.chunk.js"
+  },
+  {
+    "revision": "b201645607535c131a93",
+    "url": "./static/js/runtime-main.392b00ed.js"
+  }
+]);
--- src/helper/random.js
+++
@@ -1,5 +0,0 @@
-const getRndInteger = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-export { getRndInteger };
