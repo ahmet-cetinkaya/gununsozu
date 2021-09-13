58f31dd14bf4f352ac325a40b9a1f0511be0a091
Ahmet Çetinkaya <ahmet4cetinkaya@outlook.com>
2021-09-13 20:21:35 +0300

    Updates

---
+++ precache-manifest.ddb33f41a89251175ea72fe880b04b94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "983dab9255fd0b9b66c88d99e594bc13",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d0878882ddbccc03421d",
+    "url": "./static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "576ba5972d941b252588",
+    "url": "./static/js/2.dfc1031c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.dfc1031c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0878882ddbccc03421d",
+    "url": "./static/js/main.a82a22e0.chunk.js"
+  },
+  {
+    "revision": "b201645607535c131a93",
+    "url": "./static/js/runtime-main.392b00ed.js"
+  }
+]);
